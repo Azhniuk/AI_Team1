bf0c6bb881e1128f125151fc5b84915fa80612a3
Matthieu Porte <matthieu@famille-porte.eu>
2024-03-20 11:38:45 +0200

    started creation of child

--- algo/gameTree.py
+++ algo/gameTree.py
@@ -3,43 +3,72 @@ from typing import Optional, Dict, List
 
 # current number
 # bank points
-# p1 points
-# p2 points
-State = [int, int, int , int]
+# p1(human) points
+# p2(bot) points
+State = [int, int,int ,int]
 
 
+StartNumber = 14334
+
+StartState = [StartState,0,0,0]
 
 class Node:
-    def __init__(self, state: State, parent: Optional['Node'] = None):
+    def __init__(self, state: State, humanTurn: bool,parent: Optional['Node'] = None):
         self.children: List[Node] = []
         self.state: State = state
-        self.is_dead_end: bool = False
+        self.humanTurn: bool = humanTurn
         self.parent: Optional[Node] = parent
 
     #function that generates the descendants of the node
     def expand(self):
         if self.is_terminal():
             return
-        #TODO
+        # check if divisible by 2
+        if self.state[0] % 2 == 0:
+            newnumber = self.state[0]//2
+            newbank = self.state[1]
+            hum = self.state[2]
+            bot = self.state[3]
+
+            if newnumber%2 == 0: # even
+                if self.humanTurn:
+                    hum += 1
+                else: 
+                    bot += 1
+            else: # odd 
+                if self.humanTurn:
+                    hum -= 1
+                else:
+                    bot -= 1
+
+            if newnumber%5 == 0:
+                newbank+=1
+
+            child1 = [newnumber,newbank,hum,bot]
+
+
+        # check if divisible by 3
+        if self.state[0] % 3 == 0:
+
+        # check if end node
 
 
     def is_terminal(self) -> bool:
-        if len(self.state[1]) == 0 and self.state[0] in [0,2,3,5]:
+        if self.state[0] in [0,2,3,5]:
             return True
         return False
 
     def __print__(self):
         print("Current number :",self.state[0])
-        print("Next numbers   :",self.state[1])
-        print("Bank points    :",self.state[2])
-        print("P1 points      :",self.state[3])
-        print("P2 points      :",self.state[4])
+        print("Bank points    :",self.state[1])
+        print("P1 points      :",self.state[2])
+        print("P2 points      :",self.state[3])
 
 
 class GameTree:
 
     def __init__(self):
-        #self.root = #TODO
+        self.root = Node(StartState)
         self.node_count = 0
 
     def generate_tree(self):
